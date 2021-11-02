20768c423cd3fd25c7a6fb2bde05cd29d0087b70
Mike Solomon <mike@meeshkan.com>
2021-11-02 16:32:59 +0200

    LFO

--- src/FPSound/C02/FadeNoteLFO.purs
+++ src/FPSound/C02/FadeNoteLFO.purs
@@ -10,7 +10,7 @@ import Wags.Learn.Oscillator (lfo)
 import WAGS.Lib.Learn.Note (note_)
 
 bending :: Number -> Number
-bending time = 0.3 + lfo { phase: 0.0, amp: 0.3, freq: 2.0 } time
+bending = add 0.3 <<< lfo { phase: 0.0, amp: 0.3, freq: 2.0 }
 
 main :: Player
 main = player (note_ (Volume bending) breve c4)
