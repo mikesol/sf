882be7f0aa1974f76726f3c559066052adf29ea4
Mike Solomon <mike@meeshkan.com>
2021-11-03 22:22:54 +0200

    Adds common footer

--- src/FPSound/Intro/Intro.purs
+++ src/FPSound/Intro/Intro.purs
@@ -11,15 +11,15 @@ import WAGS.Lib.Tidal (AFuture, tdl)
 import WAGS.Lib.Tidal.Tidal (lnr, lnv, make, onTag, parse_, s)
 import WAGS.Lib.Tidal.Types (BufferUrl(..), Sample(..))
 
-m2 = 4.0 * 1.0 * 60.0 / 111.0 :: Number
-
 wag :: AFuture
 wag =
-  make m2
+  make (4.0 * 1.0 * 60.0 / 111.0)
     { earth: s
         """bass:1 msg hh ~ [hh:6 bass:0] msg:2 hh ~ ,
-    ~ chin*4 ~ ~ ~ tech:0*2 ~ ~ , ~ ~ hh:2 ~ ~ newnotes [~ newnotes:2] ~  ,
-    ~ ~ ~ ~ hh:7 ~ ~ tech:2*4 , ~ ~ ~ ~ ~ ~ ~ chin:1*2 ,
+    ~ chin*4 ~ ~ ~ tech:0*2 ~ ~ ,
+    ~ ~ hh:2 ~ ~ newnotes [~ newnotes:2] ~  ,
+    ~ ~ ~ ~ hh:7 ~ ~ tech:2*4 ,
+    ~ ~ ~ ~ ~ ~ ~ chin:1*2 ,
     pad:3"""
     , wind: s
         $ onTag "comp"
@@ -31,7 +31,10 @@ wag =
             )
         $ onTag "comp"
             ( set (_Just <<< lnr)
-                (add 0.95 <<< mul 0.1 <<< _.initialEntropy <<< unwrap)
+                $ add 0.95 
+                    <<< mul 0.1
+                    <<< _.initialEntropy
+                    <<< unwrap
             )
         $ parse_ "wdm:1;comp"
     , sounds: Map.fromFoldable $ map (\(a /\ b) -> Sample a /\ BufferUrl b)
