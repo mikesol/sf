4bbf4bcaf74058f32fe5f3c48570022456680e60
Mike Solomon <mike@meeshkan.com>
2021-11-04 11:08:34 +0200

    Adds next link

--- src/components/next.jsx
+++ src/components/next.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { Link } from "gatsby"
 export default ({next}) => (
   <div style={{display:"flex", justifyContent:"space-between",flexDirection:"row"}}>
     <div></div>
-    <a href={next}>Next</a>
+    <Link to={next}>Next</Link>
   </div>
 );
