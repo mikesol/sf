a2c47523050419d7e0061551062e2d644c0a2759
Mike Solomon <mike@meeshkan.com>
2021-11-04 10:48:45 +0200

    Prettier

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,10 @@ import React from "react";
 export default () => (
   <blockquote>
     <p>
-      <a href="https://github.com/mikesol/purescript-wags">Click here</a> for
-      additional resources for working with these examples on the cloud, on
-      Windows, on Mac or on Linux.{" "}
-      <a href="https://purescript.org">Click here</a> to learn more about the
-      PureScript programming language, and join the Discord using{" "}
+      <a href="https://github.com/mikesol/wagsi">Click here</a> to learn more
+      about working with these examples on the cloud, on Windows, on Mac or on
+      Linux. <a href="https://purescript.org">Click here</a> to learn more about
+      the PureScript programming language, and join the Discord using{" "}
       <a href="https://purescript.org/chat">this link</a>. You'll find me on the{" "}
       <code>#music</code> channel 🎶
     </p>
