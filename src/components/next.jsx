892d97040f93f1b64a891e8db5aea51dc8079549
Mike Solomon <mike@meeshkan.com>
2021-11-04 11:00:48 +0200

    Adds next link

---
+++ src/components/next.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default ({next}) => (
+  <div style={{display:"flex", justifyContent:"space-between",flexDirection:"row"}}>
+    <div></div>
+    <a href={next}>Next</a>
+  </div>
+);
