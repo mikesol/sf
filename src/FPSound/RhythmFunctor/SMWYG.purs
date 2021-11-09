20b9bef1b5c3a61f4ea4374370f22618f3506684
Mike Solomon <mike@meeshkan.com>
2021-11-09 15:23:36 +0200

    Adds ff

---
+++ src/FPSound/RhythmFunctor/SMWYG.purs
@@ -0,0 +1,9 @@
+module FPSound.RhythmFunctor.SMWYG where
+
+import WAGS.Lib.Learn (Player, player)
+
+smyg :: String
+smyg = "https://media.graphcms.com/B98L69ZNRWOV86ZHO7K9"
+
+main :: Player
+main = player smyg
