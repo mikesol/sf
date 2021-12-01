462f176259bcba5756a9b7abdfd087e6b832f5ff
Mike Solomon <mike@wavr.so>
2021-12-01 08:41:09 +0200

    Adds function example

---
+++ src/FPSound/CrashCourse/FofT.purs
@@ -0,0 +1,36 @@
+module FPSound.CrashCourse.FofT where
+
+import Prelude
+
+import WAGS.Lib.Learn (Player, player)
+import WAGS.Lib.Learn.Oscillator (lfo)
+import WAGS.Lib.Tidal (AFuture, tdl)
+import WAGS.Lib.Tidal.Tidal as T
+
+wag :: AFuture
+wag = T.make 4.0
+  { earth: T.s
+      $ T.onTag "ph"
+          ( T.changeRate
+              ( _.sampleTime
+                  >>> mul 1.3
+                  >>> add 1.0
+                  >>> min 1.2
+              )
+          )
+      $ map
+          ( T.changeVolume
+              ( _.clockTime
+                  >>> lfo
+                    { phase: 0.0
+                    , freq: 4.0
+                    , amp: 0.4
+                    }
+                  >>> add 0.4
+              )
+          )
+      $ T.parse_ "[psr:3 ~] [~ psr:3;ph ~ psr:3;ph ~]"
+  }
+
+main :: Player
+main = player (tdl wag)
---
+++ src/FPSound/CrashCourse/Sounds.purs
@@ -0,0 +1,26 @@
+module FPSound.CrashCourse.Sounds where
+
+import Prelude
+
+import Data.Tuple.Nested ((/\))
+import Foreign.Object (fromFoldable)
+import WAGS.Lib.Learn (Player, player)
+import WAGS.Lib.Tidal (AFuture, tdl)
+import WAGS.Lib.Tidal.Tidal (make, s)
+import WAGS.Lib.Tidal.Types (BufferUrl(..))
+
+wag :: AFuture
+wag = make 4.0
+  { earth: s "lowhit highhit"
+  , sounds: fromFoldable $
+      (map <<< map)
+        ( BufferUrl
+            <<< append "https://media.graphcms.com/"
+        )
+        [ "lowhit" /\ "I54aFAuRoq05CHlcK4aj"
+        , "highhit" /\ "1fLcZIK9Twq8scW2Pv2G"
+        ]
+  }
+
+main :: Player
+main = player (tdl wag)
